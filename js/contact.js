23bfd27e8dee0ea16d1feb3aeab9049de9a43d53
Zafar201 <ayshasawaab@gmail.com>
2021-04-17 22:38:35 +0530

    first finish

--- js/contact.js
+++ js/contact.js
@@ -1,36 +1,39 @@
-$(function()
-{
-    $("#contact form").submit(function()
-    {
-        var form = $(this);
-        var str = form.serialize();
-        $.ajax(
-        {
-            type: "POST",
-            url: "contact.php",
-            data: str,
-            success: function(msg)
-            {
-                $('.card').css('height', 'auto'); // fix
-                $("#contact .result .alert").remove();
-                msg = JSON.parse(msg);
+// $(function()
+// {
+//     $("#contact form").submit(function()
+//     {
+//         var form = $(this);
+//         var str = form.serialize();
+//         $.ajax(
+//         {
+//             type: "POST",
+//             url: "https://script.google.com/macros/s/AKfycbwQYf0ZrxkB7UhTAHEKxY6xiGZhpep7xAJsXeRumjh4r5qNAeqaHv480x4fJEf2Kbd0dA/exec",
+//             data: str,
+//             success: function(msg)
+//             {
+//                 $('.card').css('height', 'auto'); // fix
+//                 $("#contact .result .alert").remove();
+//                 msg = JSON.parse(msg);
 
-                if(msg.status == 'OK')
-                {
-                    $('#contact .result').append('<div class="alert alert-success">Your message has been sent. Thank you!<button type="button" class="close" data-dismiss="alert">×</button></div');
-                }
-                else if(msg.text)
-                {
-                    $.each(msg.text, function(i, elem){
-                        $('#contact .result').append('<div class="alert alert-error">' + elem + '<button type="button" class="close" data-dismiss="alert">×</button></div');
-                    })
-                }
-                else
-                {
-                    $('#contact .result').append('<div class="alert alert-error">Error<button type="button" class="close" data-dismiss="alert">×</button></div');
-                }
-            }
-        })
-        return false;
-    })
-})
+//                 if(msg.status == 'OK')
+//                 {
+//                     $('#contact .result').append('<div class="alert alert-success">Your message has been sent. Thank you!<button type="button" class="close" data-dismiss="alert">×</button></div');
+//                 }
+//                 else if(msg.text)
+//                 {
+//                     $.each(msg.text, function(i, elem){
+//                         $('#contact .result').append('<div class="alert alert-error">' + elem + '<button type="button" class="close" data-dismiss="alert">×</button></div');
+//                     })
+//                 }
+//                 else
+//                 {
+//                     $('#contact .result').append('<div class="alert alert-error">Error<button type="button" class="close" data-dismiss="alert">×</button></div');
+//                 }
+//             }
+//         })
+//         return false;
+//     })
+// })
+
+
+	
--- js/supersized.images.js
+++ js/supersized.images.js
@@ -1,4 +1,6 @@
-			jQuery(function($){
+
+		
+		jQuery(function($){
 				
 				$.supersized({
 				
@@ -8,7 +10,7 @@
 					start_slide             :   1,			// Start slide (0 is random)
 					stop_loop				:	0,			// Pauses slideshow on last slide
 					random					: 	0,			// Randomize slide order (Ignores start slide)
-					slide_interval          :   12000,		// Length between transitions
+					slide_interval          :   2000,		// Length between transitions
 					transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
 					transition_speed		:	1000,		// Speed of transition
 					new_window				:	1,			// Image links open in new window/tab
@@ -31,11 +33,10 @@
 					thumb_links				:	0,			// Individual thumb links for each slide
 					thumbnail_navigation    :   0,			// Thumbnail navigation
 					slides 					:  	[			// Slideshow Images
-														{image : './images/slider/banner2.jpeg', title : 'Maleha Desert <div class="slidedescription">I took this photo in Maleha Desert Located in UAE -  Sharjah with MY Sony Alpha 7 Mark III . I was amazed by quality of the image especially that I captured with the kit lens FE 28-70 f/3.5 - 5.6 </div>', thumb : '', url : 'http://themes.tvda.eu/'},
-														{image : './images/slider/banner3.jpeg', title : '"Fujeriah Mountains"<div class="slidedescription">This how it looks like the sunrise view from the top of fujeriah mountains.I spent the night  camping there then I took my tripod and my Sony to the top just to capture this moment. </div>', thumb : '', url : 'http://themes.tvda.eu/'},
-														{image : './images/slider/banner4.jpeg', title : 'Poli <div class="slidedescription">This Photo was taken during a crispy night of the majestic Dubai Creek Harbour  with my favourite Sony 85 - f1.8 lens</div>', thumb : '', url : 'http://themes.tvda.eu/'},
-														{image : './images/slider/banner9.jpeg', title : 'Hatta Dam <div class="slidedescription">Photo of Hatta Dam in Dubai, I "ve captured this one with Sony alpha 7 mark III and tamron 17-28 f-2.8 lens </div>', thumb : '', url : 'http://themes.tvda.eu/'},
-													
+						{image : './images/slider/banner2.jpeg', title : 'Maleha Desert <div class="slidedescription">I took this photo in Maleha Desert Located in UAE -  Sharjah with MY Sony Alpha 7 Mark III . I was amazed by quality of the image especially that I captured with the kit lens FE 28-70 f/3.5 - 5.6 </div>', thumb : '', url : 'http://themes.tvda.eu/'},
+						{image : './images/slider/banner3.jpeg', title : '"Fujeriah Mountains"<div class="slidedescription">This how it looks like the sunrise view from the top of fujeriah mountains.I spent the night  camping there then I took my tripod and my Sony to the top just to capture this moment. </div>', thumb : '', url : 'http://themes.tvda.eu/'},
+						{image : './images/slider/banner7.jpeg', title : 'The Sheikh Zayed Grand Mosque <div class="slidedescription">A majestic sunset for a majestic mosque Located in Abu Dhabi, UAE</div>', thumb : '', url : 'http://themes.tvda.eu/'},
+						{image : './images/slider/banner9.jpeg', title : 'Hatta Dam <div class="slidedescription">Photo of Hatta Dam in Dubai, I "ve captured this one with Sony alpha 7 mark III and tamron 17-28 f-2.8 lens </div>', thumb : '', url : 'http://themes.tvda.eu/'},
 												],
 												
 					// Theme Options			   
